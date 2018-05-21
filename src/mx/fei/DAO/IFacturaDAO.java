13db5a739fbf39267c1973cdc909d814e7b08a8a
José Espinoza <36683334+betoes@users.noreply.github.com>
2018-05-21 14:48:34 -0500

    Dao de factura y tarjeta credito

---
+++ src/mx/fei/DAO/IFacturaDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.administracion.vehiculos;
+
+/**
+ *
+ * @author dell
+ */
+public interface IFacturaDAO {
+    
+    public boolean crearFactura(Factura factura);
+    
+}
